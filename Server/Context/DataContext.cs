d48a841fd0159edbbfdf8a66d6d65a05c98ccc76
Batuhan <batuhanyavuz467@gmail.com>
2023-06-23 08:23:46 +0300

    Add project files.

---
+++ Server/Context/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using HospitalApp.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalApp.Server.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Meet> Meets { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Policlinic> Policlinics { get; set;}
+    }
+}
